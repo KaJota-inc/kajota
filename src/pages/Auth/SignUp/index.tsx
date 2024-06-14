454c9f367024a684f004c241f4b503b0b70e230a
oola <oola@oolas-MacBook-Pro.local>
2024-06-14 04:48:41 +0100

    feat: sign up screen done

--- src/pages/Auth/SignUp/index.tsx
+++ src/pages/Auth/SignUp/index.tsx
@@ -1,16 +1,238 @@
-import {StyleSheet} from "react-native";
+import {ScrollView, StatusBar, StyleSheet, TouchableOpacity} from "react-native";
 
 import React from "react";
 import {Text, View} from "@components/Themed";
+import {MainButton} from "@components/index";
+import {AuthProps, AuthRoutes} from "@shared/const/routerAuth";
+import {COLORS, SIZES} from "@constants/Colors";
+import {AppleLogoSVG, EmailLogoSVG, GoogleLogoSVG} from "@shared/components/SVGS";
 
-const SignUp = () => {
+type NavigationProps = AuthProps<AuthRoutes.SignUp>;
+
+
+const SignUp: React.FC<NavigationProps> = ({navigation}) => {
     return (
-        <View>
-            <Text>Highlights</Text>
+        <View style={styles.main}>
+            <StatusBar barStyle="dark-content"/>
+            <View style={styles.container}>
+                <View style={styles.subContainer}>
+                    <ScrollView
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={styles.scrollContent}
+                        style={styles.scroll}
+                    >
+                        <View style={styles.r1}>
+                            <Text style={styles.r1t2}>Start your journey with Jota!</Text>
+                        </View>
+
+                        <View style={styles.r3}>
+                            <Text style={styles.r3t1}>Please select your desired signup option</Text>
+                        </View>
+
+                        <View style={styles.r8}>
+                            <MainButton
+                                title={
+                                <View style={styles.r8t3}>
+                                    <AppleLogoSVG/>
+                                    <Text style={styles.r8t4}>
+                                        Continue with Apple
+                                    </Text>
+                                </View>
+                                }
+                                onPressFunction={() => {
+                                }}
+                                err={false}
+                                btnStyle={styles.r8t1}
+                            />
+                        </View>
+                        <View style={styles.r8}>
+                            <MainButton
+                                title={
+                                    <View style={styles.r8t3}>
+                                        <GoogleLogoSVG/>
+                                        <Text style={styles.r8t5}>
+                                            Continue with Google
+                                        </Text>
+                                    </View>
+                                }
+                                onPressFunction={() => {
+                                }}
+                                err={false}
+                                btnStyle={styles.r8t2}
+                            />
+                        </View>
+                        <View style={styles.r8}>
+                            <MainButton
+                                title={
+                                    <View style={styles.r8t3}>
+                                        <EmailLogoSVG/>
+                                        <Text style={styles.r8t5}>
+                                            Continue with Email
+                                        </Text>
+                                    </View>
+                                }
+                                onPressFunction={() => {
+                                }}
+                                err={false}
+                                btnStyle={styles.r8t2}
+                            />
+                        </View>
+
+                        <View style={styles.r7}>
+                            <Text style={styles.r7t1}>Already have an account? </Text>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    navigation?.navigate(AuthRoutes.SignIn);
+                                }}
+                            >
+                                <Text style={styles.r7t2}> Sign In</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </ScrollView>
+
+                </View>
+            </View>
+
+
         </View>
     );
 };
 
 export default SignUp;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        // borderWidth: 1,
+    },
+    container: {
+        // borderWidth: 1,
+        flex: 1,
+        alignItems: "center",
+        // justifyContent: "center",
+        marginHorizontal: "5%",
+    },
+    subContainer: {
+        // flex: 1,
+        // borderWidth: 1,
+        // justifyContent: "center",
+        alignItems: "center",
+        // height: "60%",
+        width: "100%",
+        marginTop: "15%",
+        // paddingHorizontal: "5%",
+    },
+    scroll: {
+        // borderWidth: 1,
+        width: "100%",
+        marginTop: 10,
+        backgroundColor: "transparent",
+        marginBottom: 20,
+    },
+    scrollContent: {
+        // borderWidth: 1,
+        width: "100%",
+        // height: "500%",
+        alignItems: "center",
+        backgroundColor: "transparent",
+        marginBottom: 20,
+        paddingVertical: 5,
+    },
+    r1: {
+        flexDirection: "row",
+        // justifyContent: "center",
+        width: "100%",
+        // alignItems: "center",
+        marginTop: "8%",
+        marginBottom: "2%",
+        backgroundColor: "transparent",
+    },
+    r1t1: {},
+    r1t2: {
+        // marginLeft: "8%",
+        color: COLORS.light.text,
+        fontSize: SIZES.sizeNine,
+        fontWeight: "700",
+        // textAlign: "center",
+    },
+    r3: {
+        width: "100%",
+        marginTop: 5,
+        marginBottom: 15,
+    },
+    r3t1: {
+        marginBottom: 18,
+        fontWeight: "300",
+        fontSize: SIZES.sizeSeven,
+    },
+    r8: {
+        backgroundColor: "transparent",
+        marginVertical: "1%",
+        width: "100%",
+    },
+    r8t1: {
+        backgroundColor: COLORS.light.text,
+        paddingVertical: 10,
+        marginVertical: 10,
+    },
+    r8t2: {
+        backgroundColor: COLORS.light.background,
+        paddingVertical: 10,
+        marginVertical: 10,
+        borderColor: COLORS.light.text,
+        borderWidth: 1,
+    },
+    r8t3: {
+        justifyContent: "center",
+        textAlign:"center",
+        alignItems: "center",
+        flexDirection:"row",
+        backgroundColor: "transparent",
+        paddingVertical:"2%"
+    },
+    r8t4: {
+        color: COLORS.light.background,
+        justifyContent: "space-between",
+        // borderColor: COLORS.light.background,
+        textAlign:"center",
+        marginLeft: "5%",
+        alignItems: "center",
+        marginTop: "2%",
+        height:"100%",
+        fontSize: SIZES.sizeSeven,
+        fontWeight: "500",
+    },
+    r8t5: {
+        color: COLORS.light.text,
+        justifyContent: "space-between",
+        // borderColor: COLORS.light.background,
+        textAlign:"center",
+        marginLeft: "5%",
+        alignItems: "center",
+        marginTop: "1%",
+        height:"100%",
+        fontSize: SIZES.sizeSeven,
+        fontWeight: "500",
+    },
+    r7: {
+        width: "100%",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        marginTop: "7%",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 20,
+    },
+    r7t1: {
+        color: COLORS.light.text,
+        fontSize: SIZES.sizeSixC,
+        fontWeight: "500",
+        textAlign: "center",
+    },
+    r7t2: {
+        color: COLORS.light.colorOne,
+        fontSize: SIZES.sizeSixC,
+        fontWeight: "500",
+        textAlign: "center",
+    },
+});
--- src/shared/components/SVGS.tsx
+++ src/shared/components/SVGS.tsx
@@ -2,6 +2,9 @@ import {StyleSheet} from "react-native";
 import React from "react";
 
 import FifaLogo from "../assets/images/svg/FIFA_logo_without_slogan 1.svg";
+import AppleLogo from "../assets/images/svg/apple.svg";
+import GoogleLogo from "../assets/images/svg/google.svg";
+import EmailLogo from "../assets/images/svg/email_icon.svg";
 import {COLORS} from "@constants/Colors";
 
 export const FifaLogoSVG =
@@ -18,6 +21,63 @@ export const FifaLogoSVG =
                     width={width}
                     fill={color}
                     stroke={stroke}
+                />
+            </>
+        );
+    };
+
+export const AppleLogoSVG =
+    ({
+         height = 25,
+         width = 25,
+         color = COLORS.light.background,
+         // stroke = COLORS.light.background
+     }) => {
+        return (
+            <>
+                <AppleLogo
+                    height={height}
+                    width={width}
+                    fill={color}
+                    // stroke={stroke}
+                />
+            </>
+        );
+    };
+
+export const GoogleLogoSVG =
+    ({
+         height = 25,
+         width = 25,
+         color = COLORS.light.background,
+         // stroke = COLORS.light.background
+     }) => {
+        return (
+            <>
+                <GoogleLogo
+                    height={height}
+                    width={width}
+                    fill={color}
+                    // stroke={stroke}
+                />
+            </>
+        );
+    };
+
+export const EmailLogoSVG =
+    ({
+         height = 25,
+         width = 25,
+         color = COLORS.light.background,
+         // stroke = COLORS.light.background
+     }) => {
+        return (
+            <>
+                <EmailLogo
+                    height={height}
+                    width={width}
+                    fill={color}
+                    // stroke={stroke}
                 />
             </>
         );
